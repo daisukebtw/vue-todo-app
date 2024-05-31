ee2c7f3efc6564bf6cc3f6a650f62847a8242d62
krishnasankar <sankarkrishna689@gmail.com>
2024-05-31 17:44:30 +0530

    Update: Help section updated.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-center fixed text-xs lg:text-sm p-2 bottom-0 w-full h-10 bg-dark flex flex-row items-center justify-center gap-4 pl-4 lg:pl-8 pr-4 lg:pr-8 z-50">
+    <div class="text-center fixed text-xs lg:text-sm bottom-0 w-full h-10 bg-dark flex flex-row items-center justify-center gap-4 p-8 z-50">
         <span class="font-PoppinsBold flex-1 text-start">&copy;2024</span>
         <span>Made with &#10084;&#65039; By <a href="https://krishnasankar.vercel.app" target="_blank" class="cursor-pointer font-PoppinsBold hover:text-red transition-all duration-200 lg:text-lg">Krishnasankar</a></span>
         <span class="hidden lg:block text-sm flex-1 text-end">Press <span class="bg-gray p-1 rounded-lg font-PoppinsBold">[Alt + h]</span> for Help </span>
