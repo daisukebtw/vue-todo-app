fea48b4eb2537bbf87feb0564f446f05cb36ffbb
krishnasankar <sankarkrishna689@gmail.com>
2024-06-10 13:20:40 +0530

    Changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed top-0 w-full h-20 bg-dark flex flex-row items-center justify-between z-50">
-        <h1 class="text-xl lg:text-2xl font-PoppinsBold ml-4 lg:ml-8">My<span class="text-red">Todo</span></h1>
+        <h1 class="text-xl lg:text-2xl font-PoppinsBold ml-4 lg:ml-8">My<span class="text-red">Todoo</span></h1>
         <input type="checkbox" name="online" id="online-status" v-model="onlineStatus">
         <label 
             v-if="onlineStatus" 
